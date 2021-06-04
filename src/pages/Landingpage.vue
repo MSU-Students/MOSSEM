5871838f47d0bae153bcd9a73aa35e92fbef9c50
abdulmatinmorenomaruhom <maruhomabdulmatin@gmail.com>
2021-06-04 15:58:28 +0800

    height changes

--- src/pages/Landingpage.vue
+++ src/pages/Landingpage.vue
@@ -2,14 +2,11 @@
   <q-page>
     <div class="q-gutter-md">
       <div class="row justify-between">
-        <q-parallax
+        <q-parallax 
           src="~assets/SP logo44(cover).jpg"
-          class="cover-photo"
-          :height="screenHeight"
-        >
+            :height="750"        >
           <h1 class="text-white">
             Sining Pananadem
-            
           </h1>
         </q-parallax>
       </div>
@@ -20,20 +17,11 @@
 <script>
 export default {
   data() {
-    return {
-      screenHeight: 0
-    };
+    return {}
   },
-  created() {
-      this.screenHeight = screen.height - 107;
-      console.log(this.screenHeight)
-  }
 };
 </script>
 
 <style scopes>
-.cover-photo {
-  height: 100vh;
-  width: 100vw;
-}
+
 </style>
