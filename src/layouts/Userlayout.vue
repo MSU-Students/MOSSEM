7ab7fc3af9639d19097888fd0918faad36bc85d4
abdulmatinmorenomaruhom <maruhomabdulmatin@gmail.com>
2021-06-06 00:02:44 +0800

    Delete Comments

--- src/layouts/Userlayout.vue
+++ src/layouts/Userlayout.vue
@@ -9,26 +9,7 @@
           Landing Page
         </q-toolbar-title>
       </q-toolbar>
-
-      <!-- <q-tabs align="left" >
-        <q-route-tab 
-        to="/page1" 
-        label="Page One" 
-        
-        />
-        <q-route-tab to="/page2" label="Page Two" />
-        <q-route-tab to="/page3" label="Page Three" />
-      </q-tabs> -->
-      <!-- <q-card-actions>
-          <q-btn
-            class="full-width"
-            label="login"
-            color="white"
-            text-color="red"
-            to="/"
-          >
-          </q-btn>
-        </q-card-actions> -->
+ 
     </q-header>
 
     <q-footer class="bg-grey-8 text-white">
